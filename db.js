ba2c989b82f3513e9cbf4d7c211d45e29a896551
andrewmulya22 <56527292+andrewmulya22@users.noreply.github.com>
2020-12-26 19:03:46 +0700

    Update db.js

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql')
 
 const pool = mysql.createPool({
     host: 'us-cdbr-east-02.cleardb.com',
-    user: 'b30806d94c061f',
-    password: 'a65d272a',
-    database: 'heroku_af9caaf069edf81',
+    user: '',
+    password: '',
+    database: '',
     charset: 'utf8'
 })
 
